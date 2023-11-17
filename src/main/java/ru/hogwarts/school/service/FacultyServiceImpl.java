ead625c8b7953c3bfe1c36a6f71c69ee6079304e
kimlevcenko <levchenko.kim@icloud.com>
2023-11-17 18:55:47 +0500

    hw_3_course_improve_app init commit

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -2,7 +2,6 @@ package ru.hogwarts.school.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.model.Faculty;
